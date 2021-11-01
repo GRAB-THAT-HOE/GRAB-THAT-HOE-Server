9ac408c6537d796b71dddaf06ba41205ad468806
whitebear05 <woojs0505@naver.com>
2021-11-01 12:30:26 +0900

    Add: Posting model

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
+import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
-import session from "express-session";
-import flash from "express-flash";
-import MongoStore from "connect-mongo";
+import cors from "cors";
 
 const app = express();
 const logger = morgan("dev");
@@ -10,18 +9,8 @@ const logger = morgan("dev");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-  session({
-    secret: process.env.COOKIE_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 86400000,
-    },
-    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
-  })
-);
-app.use(flash());
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
 app.use("/", (req, res) => {
   return res.send("Hi!!!");
 });
