07b98c646b38580cdbbd0e69e70b3c8434bd018d
whitebear05 <woojs0505@naver.com>
2021-11-12 14:41:53 +0900

    [UPDATE] pin router

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import auth from "./auth";
 import post from "./post";
+import pin from "./pin";
 
 const router = Router();
 
 router.use("/auth", auth);
 router.use("/post", post);
+router.use("/pin", pin);
 
 export default router;
