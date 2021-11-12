4857fc6835c2d94c70f6e5570beba1554e300adb
whitebear05 <woojs0505@naver.com>
2021-11-12 14:39:53 +0900

    [ADD] get my pins controller

--- src/api/v1/pin/index.ts
+++ src/api/v1/pin/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import postPin from "./pin.ctrl/postPin";
 import { verifyToken } from "../../../middlewares";
+import getMyPins from "./pin.ctrl/getMyPins";
 
 const router = Router();
+
+router.get("/my", verifyToken, getMyPins);
 
 router.post("/:idx", verifyToken, postPin);
 
