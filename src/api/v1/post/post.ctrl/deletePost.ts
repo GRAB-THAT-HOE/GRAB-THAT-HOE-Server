8e535e6057d1cced9acd8a1a90eff6346ac0f198
whitebear05 <woojs0505@naver.com>
2021-12-16 20:18:16 +0900

    [UPDATE] deletePost controller

--- src/api/v1/post/post.ctrl/deletePost.ts
+++ src/api/v1/post/post.ctrl/deletePost.ts
@@ -1,25 +1,30 @@
 import { Response } from "express";
-import TokenRequestType from "../../../../type/TokenRequestType";
-import Post from "../../../../models/Post";
+import Post from "../../../../entity/Post";
+import { getRepository } from "typeorm";
 
 export default async (req, res: Response) => {
-  const _id: string = req.user._id;
-  const idx: string = req.params.idx;
+  const { phone } = req.user;
+  const { idx } = req.params;
   try {
-    const post = await Post.findById(idx);
+    const postRepository = getRepository(Post);
+    const post: Post = await postRepository.findOne({
+      where: {
+        id: idx,
+      },
+    });
     if (!post) {
       return res.status(404).json({
         status: 404,
         message: "삭제할 포스팅을 찾지 못했습니다.",
       });
     }
-    if (String(post.owner) !== _id) {
+    if (post.user !== phone) {
       return res.status(403).json({
         status: 403,
         message: "포스팅을 삭제할 권한이 없습니다.",
       });
     }
-    await Post.findByIdAndDelete(post._id);
+    await postRepository.remove(post);
     return res.status(200).json({
       status: 200,
       message: "포스팅 삭제에 성공했습니다.",
